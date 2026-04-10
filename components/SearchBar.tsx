ec38eb857de6e845ae6d3e17bfe3ce418578548e
HabibEniolabi <habibullahayomide009@gmail.com>
2026-04-10 08:39:10 +0100

    additionn of the search components

---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { View, Image, TextInput } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+interface Props {
+    onPress?: () => void;
+    placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className='flex-row bg-dark-200 rounded-full px-5 py-4 items-center'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#AB8BFF" />
+        <TextInput 
+            onPress={onPress}
+            placeholder={placeholder}
+            value=''
+            onChange={() => {}}
+            placeholderTextColor="#A8B5DB"
+            className='flex-1 mal-2 text-white'
+        />
+    </View>
+  )
+}
+
+export default SearchBar
