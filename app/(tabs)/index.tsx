d43f857fd0189552baaf7200ba734a3ce67a833f
HabibEniolabi <habibullahayomide009@gmail.com>
2026-04-11 12:15:21 +0100

    fixes: api/endoint fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, View, Text } from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
@@ -15,12 +23,26 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBlock: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push("/search")}
-            placeholder="Search through 300+ movies online"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search through 300+ movies online"
+            />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">latest movies</Text>
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
