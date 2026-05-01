b6f4caac3a4be1e47655c2fe0221c209ad65d7ed
HabibEniolabi <habibullahayomide009@gmail.com>
2026-05-01 13:33:25 +0100

    modify: addition of onboarding/genere screen

--- app/onboarding/genres.tsx
+++ app/onboarding/genres.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import OnboardingHeader from "@/components/OnboardingHeader";
+import OnboardingHeaderInfo from "@/components/OnboardingHeaderInfo";
 
 const Genres = () => {
   return (
-    <View className='bg-primary flex-1'>
-      <Text className='text-white my-auto mx-auto'>Genres</Text>
+    <View className="bg-primary flex-1">
+      <View className="flex mt-16 flex-col">
+        <OnboardingHeader step={3} />
+        <ScrollView
+          className="mt-8"
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 80 }}
+        >
+          <OnboardingHeaderInfo
+            title={"What do you love? ❤️"}
+            subtitle={
+              "Pick at least 3 genres so we can personalise your feed."
+            }
+          />
+        </ScrollView>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Genres
+export default Genres;
