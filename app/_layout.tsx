626f3d70a5b60bd953a1956678e12e530614157b
HabibEniolabi <habibullahayomide009@gmail.com>
2026-05-06 15:46:06 +0100

    fixes:setting statusBar to true

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
 import { Stack } from "expo-router";
 import "./globals.css";
-import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="index" />
         <Stack.Screen
