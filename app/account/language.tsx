c4cec4df1d0fb8904cd3250afe3597eab3c3ba8d
HabibEniolabi <habibullahayomide009@gmail.com>
2026-05-15 08:22:22 +0100

    addition of languauges

--- app/account/language.tsx
+++ app/account/language.tsx
@@ -1,12 +1,161 @@
-import { View, Text, TouchableOpacity, Alert, TextInput, FlatList } from "react-native";
+// import { View, Text, TouchableOpacity, Alert, TextInput, FlatList } from "react-native";
+// import React, { useMemo, useState } from "react";
+// import { router } from "expo-router";
+// import Feather from "react-native-vector-icons/Feather";
+// import { useTranslation } from "react-i18next";
+
+// import i18n, { changeAppLanguage } from "@/interfaces/i18n";
+// import { LANGUAGES } from "@/services/languages";
+// import { LinearGradient } from "expo-linear-gradient";
+// import LanguageCard from "@/components/LanguageCard";
+
+// const Language = () => {
+//   const { t } = useTranslation();
+
+//   const currentLanguage = i18n.language || "en";
+
+//   const [search, setSearch] = useState("");
+//   const [selectedLanguage, setSelectedLanguage] = useState(currentLanguage);
+
+//   const currentLanguageData = LANGUAGES.find(
+//     (item) => item.code === currentLanguage
+//   );
+
+//   const hasLanguageChanged = selectedLanguage !== currentLanguage;
+
+//   const otherLanguages = useMemo(() => {
+//     const searchValue = search.trim().toLowerCase();
+
+//     return LANGUAGES.filter((item) => {
+//       const isNotCurrent = item.code !== currentLanguage;
+
+//       const matchesSearch =
+//         item.name.toLowerCase().includes(searchValue) ||
+//         item.nativeName.toLowerCase().includes(searchValue);
+
+//       return isNotCurrent && matchesSearch;
+//     });
+//   }, [search, currentLanguage]);
+
+//   const handleApplyLanguage = async () => {
+//     if (!hasLanguageChanged) {
+//       router.back();
+//       return;
+//     }
+//     try {
+//       await changeAppLanguage(selectedLanguage);
+
+//       Alert.alert("Success", "Language updated successfully");
+//       router.back();
+//     } catch (error) {
+//       Alert.alert("Error", "Could not update language. Please try again.");
+//     }
+//   };
+
+//   return (
+//     <View className="bg-primary flex-1 px-5">
+//       <View className="flex-row gap-8 items-center mt-16 mb-4">
+//         <TouchableOpacity
+//           onPress={() => router.back()}
+//           activeOpacity={0.8}
+//           className="rounded-[12px] border border-[#2A2845] bg-dark-300 items-center justify-center px-2 py-2"
+//         >
+//           <Feather name="chevron-left" size={24} color="#8B88A8" />
+//         </TouchableOpacity>
+//         <Text className="text-white font-bold text-[24px] text-center">
+//           {t("language")}
+//         </Text>
+//       </View>
+//       <View className="h-[70px] rounded-[22px] border border-[#2A2845] bg-dark-300 flex-row items-center px-5 mb-8">
+//         <Feather name="search" size={26} color="#5D5A7A" />
+
+//         <TextInput
+//           value={search}
+//           onChangeText={setSearch}
+//           placeholder={t("searchLanguage")}
+//           placeholderTextColor="#5D5A7A"
+//           className="flex-1 text-white text-[20px] ml-3 font-semibold"
+//         />
+//       </View>
+//       <FlatList
+//         data={otherLanguages}
+//         keyExtractor={item => item.code}
+//         showsVerticalScrollIndicator={false}
+//         ListHeaderComponent={
+//           <View>
+//             {/* Current */}
+//             {currentLanguage && (
+//               <View className="mb-6">
+//                 <Text className="text-[#5D5A7A] font-bold tracking-[3px] text-[14px] mb-4">
+//                   {t('current').toUpperCase()}
+//                 </Text>
+
+//                 <LanguageCard
+//                   item={currentLanguageData}
+//                   selected={selectedLanguage === currentLanguageData.code}
+//                   onPress={() => setSelectedLanguage(currentLanguageData.code)}
+//                 />
+//               </View>
+//             )}
+
+//             <Text className="text-[#5D5A7A] font-bold tracking-[3px] text-[14px] mb-4">
+//               {t('allLanguages').toUpperCase()}
+//             </Text>
+//           </View>
+//         }
+//         renderItem={({ item }) => (
+//           <LanguageCard
+//             item={item}
+//             selected={selectedLanguage === item.code}
+//             onPress={() => setSelectedLanguage(item.code)}
+//           />
+//         )}
+//         contentContainerStyle={{
+//           paddingBottom: 120,
+//         }}
+//       />
+
+//       <View className="absolute bottom-8 left-5 right-5">
+//         <TouchableOpacity activeOpacity={0.85} onPress={handleApplyLanguage}>
+//           <LinearGradient
+//             colors={["#D83EBE", "#9B4DFF"]}
+//             start={{ x: 0, y: 0 }}
+//             end={{ x: 1, y: 0 }}
+//             style={{
+//               height: 78,
+//               borderRadius: 28,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             <Text className="text-white text-[24px] font-bold">
+//               {t("applyLanguage")}
+//             </Text>
+//           </LinearGradient>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// };
+// export default Language;
+
+
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+  FlatList,
+} from "react-native";
 import React, { useMemo, useState } from "react";
 import { router } from "expo-router";
 import Feather from "react-native-vector-icons/Feather";
 import { useTranslation } from "react-i18next";
+import { LinearGradient } from "expo-linear-gradient";
 
 import i18n, { changeAppLanguage } from "@/interfaces/i18n";
 import { LANGUAGES } from "@/services/languages";
-import { LinearGradient } from "expo-linear-gradient";
 import LanguageCard from "@/components/LanguageCard";
 
 const Language = () => {
@@ -17,28 +166,38 @@ const Language = () => {
   const [search, setSearch] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState(currentLanguage);
 
-  const selectedLanguageData = LANGUAGES.find(
-    (item) => item.code === selectedLanguage
+  const currentLanguageData = LANGUAGES.find(
+    (item) => item.code === currentLanguage
   );
 
-  const otherLanguages = useMemo(() => {
+  const hasLanguageChanged = selectedLanguage !== currentLanguage;
+
+  const filteredLanguages = useMemo(() => {
+    const searchValue = search.trim().toLowerCase();
+
     return LANGUAGES.filter((item) => {
-      const isNotSelected = item.code !== selectedLanguage;
+      const isNotCurrent = item.code !== currentLanguage;
 
       const matchesSearch =
-        item.name.toLowerCase().includes(search.toLowerCase()) ||
-        item.nativeName.toLowerCase().includes(search.toLowerCase());
+        item.name.toLowerCase().includes(searchValue) ||
+        item.nativeName.toLowerCase().includes(searchValue);
 
-      return isNotSelected && matchesSearch;
+      return isNotCurrent && matchesSearch;
     });
-  }, [search, selectedLanguage]);
+  }, [search, currentLanguage]);
 
   const handleApplyLanguage = async () => {
+    if (!hasLanguageChanged) {
+      router.back();
+      return;
+    }
+
     try {
       await changeAppLanguage(selectedLanguage);
 
       Alert.alert("Success", "Language updated successfully");
-    } catch (error) {
+      // router.back();
+    } catch {
       Alert.alert("Error", "Could not update language. Please try again.");
     }
   };
@@ -53,10 +212,12 @@ const Language = () => {
         >
           <Feather name="chevron-left" size={24} color="#8B88A8" />
         </TouchableOpacity>
+
         <Text className="text-white font-bold text-[24px] text-center">
           {t("language")}
         </Text>
       </View>
+
       <View className="h-[70px] rounded-[22px] border border-[#2A2845] bg-dark-300 flex-row items-center px-5 mb-8">
         <Feather name="search" size={26} color="#5D5A7A" />
 
@@ -68,29 +229,29 @@ const Language = () => {
           className="flex-1 text-white text-[20px] ml-3 font-semibold"
         />
       </View>
+
       <FlatList
-        data={otherLanguages}
-        keyExtractor={item => item.code}
+        data={filteredLanguages}
+        keyExtractor={(item) => item.code}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View>
-            {/* Current */}
-            {selectedLanguageData && (
+            {currentLanguageData && (
               <View className="mb-6">
                 <Text className="text-[#5D5A7A] font-bold tracking-[3px] text-[14px] mb-4">
-                  {t('current').toUpperCase()}
+                  {t("current").toUpperCase()}
                 </Text>
 
                 <LanguageCard
-                  item={selectedLanguageData}
-                  selected
-                  onPress={() => setSelectedLanguage(selectedLanguageData.code)}
+                  item={currentLanguageData}
+                  selected={selectedLanguage === currentLanguageData.code}
+                  onPress={() => setSelectedLanguage(currentLanguageData.code)}
                 />
               </View>
             )}
 
             <Text className="text-[#5D5A7A] font-bold tracking-[3px] text-[14px] mb-4">
-              {t('allLanguages').toUpperCase()}
+              {t("allLanguages").toUpperCase()}
             </Text>
           </View>
         }
@@ -101,6 +262,11 @@ const Language = () => {
             onPress={() => setSelectedLanguage(item.code)}
           />
         )}
+        ListEmptyComponent={
+          <Text className="text-[#8B88A8] text-[16px] text-center mt-8">
+            No language found
+          </Text>
+        }
         contentContainerStyle={{
           paddingBottom: 120,
         }}
@@ -109,7 +275,11 @@ const Language = () => {
       <View className="absolute bottom-8 left-5 right-5">
         <TouchableOpacity activeOpacity={0.85} onPress={handleApplyLanguage}>
           <LinearGradient
-            colors={["#D83EBE", "#9B4DFF"]}
+            colors={
+              hasLanguageChanged
+                ? ["#D83EBE", "#9B4DFF"]
+                : ["#3A3758", "#3A3758"]
+            }
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
             style={{
@@ -117,10 +287,11 @@ const Language = () => {
               borderRadius: 28,
               alignItems: "center",
               justifyContent: "center",
+              opacity: hasLanguageChanged ? 1 : 0.8,
             }}
           >
             <Text className="text-white text-[24px] font-bold">
-              {t("applyLanguage")}
+              {hasLanguageChanged ? t("applyLanguage") : "Done"}
             </Text>
           </LinearGradient>
         </TouchableOpacity>
@@ -128,4 +299,5 @@ const Language = () => {
     </View>
   );
 };
+
 export default Language;
--- components/LanguageCard.tsx
+++ components/LanguageCard.tsx
@@ -1,7 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 import Feather from "react-native-vector-icons/Feather";
-
+import { LinearGradient } from "expo-linear-gradient";
 
 const LanguageCard = ({
   item,
@@ -18,42 +18,60 @@ const LanguageCard = ({
   onPress: () => void;
 }) => {
   return (
-    <TouchableOpacity
-      activeOpacity={0.85}
-      onPress={onPress}
-      className={`min-h-[75px] rounded-[24px] border px-7 mb-3 flex-row items-center justify-between ${
-        selected
-          ? 'bg-[#24104A] border-[#9B4DFF]'
-          : 'bg-dark-300 border-[#2A2845]'
-      }`}
-    >
-      <View className="flex-row items-center">
-        <Text className="text-[30px] mr-6">{item.flag}</Text>
-
-        <View>
-          <Text className="text-white text-[22px] font-bold">
-            {item.name}
-          </Text>
-
-          <Text className="text-[#7A7699] text-[18px] font-semibold mt-1">
-            {item.nativeName}
-          </Text>
-        </View>
-      </View>
-
-      <View
-        className={`w-[46px] h-[46px] rounded-full border-[3px] items-center justify-center ${
-          selected
-            ? 'bg-[#9B4DFF] border-[#9B4DFF]'
-            : 'border-[#333052]'
-        }`}
+    <TouchableOpacity activeOpacity={0.85} onPress={onPress} className="mb-3">
+      <LinearGradient
+        colors={selected ? ["#D83EBE", "#9B4DFF"] : ["#2A2845", "#2A2845"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{
+          borderRadius: 24,
+          padding: 1.5,
+        }}
       >
-        {selected && (
-          <Feather name="check" size={24} color="#FFFFFF" />
-        )}
-      </View>
+        <View
+          className={`min-h-[75px] rounded-[23px] px-6 flex-row items-center justify-between ${
+            selected ? "bg-[#24104A]" : "bg-dark-300"
+          }`}
+        >
+          <View className="flex-row items-center flex-1">
+            <Text className="text-[30px] mr-5">{item.flag}</Text>
+
+            <View className="flex-1">
+              <Text className="text-white text-[22px] font-bold">
+                {item.name}
+              </Text>
+
+              <Text className="text-[#7A7699] text-[18px] font-semibold mt-1">
+                {item.nativeName}
+              </Text>
+            </View>
+          </View>
+
+          <LinearGradient
+            colors={selected ? ["#D83EBE", "#9B4DFF"] : ["#333052", "#333052"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={{
+              width: 46,
+              height: 46,
+              borderRadius: 999,
+              padding: selected ? 0 : 3,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              className={`w-full h-full rounded-full items-center justify-center ${
+                selected ? "" : "bg-dark-300"
+              }`}
+            >
+              {selected && <Feather name="check" size={24} color="#FFFFFF" />}
+            </View>
+          </LinearGradient>
+        </View>
+      </LinearGradient>
     </TouchableOpacity>
   );
 };
 
-export default LanguageCard
+export default LanguageCard;
